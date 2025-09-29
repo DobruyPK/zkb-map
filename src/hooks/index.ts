e9ce02f9be45710d2872542e852d35e87eec09bc
dobruygames@gmail.com <you@example.com>
2025-09-29 21:07:53 +0300

    static

--- src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useKillmails, useKillmailMonitor } from './killmails'
-export { useConfiguration } from './configuration'
-export { useSolarSystems, useSolarSystemData } from './solarSystems'
-export { useConnection, useConnectionStatus } from './connection'
--- src/utils/fontawesome.ts
+++
@@ -1,6 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faCompressArrowsAlt, faExpandArrowsAlt, faWifi, faVideo, faGlobe, faList, faSlash
-} from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCompressArrowsAlt, faExpandArrowsAlt, faWifi, faVideo, faGlobe, faList, faSlash)
